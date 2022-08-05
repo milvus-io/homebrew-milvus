faca2f745e4e5128bd9b7679fbf80553c0e85fb2
Enwei Jiao <enwei.jiao@zilliz.com>
2022-08-05 16:04:31 +0800

    fix sha256

--- Formula/milvusctl.rb
+++ Formula/milvusctl.rb
@@ -2,7 +2,7 @@ class Milvusctl < Formula
   desc "Milvus k8s controller"
   homepage "https://github.com/milvus-io/milvusctl"
   url "https://github.com/milvus-io/milvusctl/releases/download/v0.1.0/milvusctl-macos"
-  sha256 "1f5aa4422bacbac22a74fe5f0a204ca1330890c3"
+  sha256 "3aeccb2324b2e9a6fd35f46944a4447b8403b2131e8fece44e70d32ba933fbdc"
 
   def install 
     bin.install "milvusctl-macos" => "milvusctl"
